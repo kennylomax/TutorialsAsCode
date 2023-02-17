3c82e456282b1f9272aaee104515eed204e1e6e5
kennylomax <kennylomax@gmail.com>
2023-02-17 10:18:43 +0100

    Autostart option

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,4 +18,24 @@ if [ -z "$KARATE_SOCAT_START" ]
 fi
 [ -z "$KARATE_WIDTH" ] && export KARATE_WIDTH="1760"
 [ -z "$KARATE_HEIGHT" ] && export KARATE_HEIGHT="1480"
-exec /usr/bin/supervisord
+
+echo "AUTO_START :"$AUTO_START
+echo "JOURNEY_NAME :"$JOURNEY_NAME
+echo "KLX1"
+
+if [ -z "$AUTO_START" ]
+then
+  echo "manual start"
+  exec /usr/bin/supervisord
+else 
+  echo "auto start"
+  exec /usr/bin/supervisord  &
+  echo "Starting sleep to allow bootup - perhaps not necessary"
+  sleep 10
+  export NOW=$(date +%s%3N)
+  cd src 
+  ls -la  
+  echo "Calling validatejourney.sh with" $JOURNEY_NAME $NOW 0 999
+  source ./validatejourney.sh $JOURNEY_NAME $NOW 0 999
+  echo "Finished"
+fi
