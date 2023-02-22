25d2ad7ca08ab6698d112f9fc9207c284f6d66f1
kennylomax <kennylomax@gmail.com>
2023-02-22 15:57:12 +0100

    Typos

--- journeys/TutorialAsCode_0.1_Template/journeysetupexample.sh
+++ journeys/TutorialAsCode_0.1_Template/journeysetupexample.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# The folder in which this journey will be built. You can override this by passing in a directory when you invoke the test
-# ./validatejourney.sh <journey>  <optional journey directory>
+# The folder in which this journey will be built.
 export MY_JOURNEY_DIR=~/journey/$NOW
+# Note you can override this by passing in a directory when you invoke the test later. 
 
-### Any environment variables that need personalizing
+# Example Environment variables that need personalizing 
 export YOUR_NAME="Your Name"
 
-# Any adjustements needed if we are running on Linux we can do here..
+# For automatic execuatons, we can handle any adjustments here..
 if [[ "$OSTYPE" == *"darwin"* ]]; then
  echo "$OSTYPE Running on Mac. ..";
 else 
-  #  For example, if you are downdloading someting in the tutorial with chrome, you would find it at /home/chrome/Downloads, so can set that variable here.
+  #  For example, if you are downdloading something in the tutorial with chrome, you would find it at /home/chrome/Downloads, so can set that variable here.
   echo "$OSTYPE Running on Linux/Debian.";
   # If on docker we want to install node ourselves..
   echo "Installing Node";
